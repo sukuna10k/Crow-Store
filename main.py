4db185e9fe93b275e743399fbd8f7f0b8305992d
sukuna10k <abdoulmadjidkingceymarkouma@gmail.com>
2025-01-02 06:26:10 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import os
 from bot import Bot
 
-# Récupérer le port depuis l'environnement ou utiliser 8000 par défaut
-port = int(os.getenv("PORT", 8000))
-
-# Lancer le bot avec le port
-Bot().run(port=port)
+Bot().run()
